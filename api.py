fab757ea2e82e1b853e8293986112141f6989129
aestampa <abigail.estampador@outlook.com>
2024-06-18 22:13:40 -0600

    Adding CORS

--- api.py
+++ api.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify, request
 from connect import connect, init_tables
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/api/projects', methods=['GET'])
 def get_project():
